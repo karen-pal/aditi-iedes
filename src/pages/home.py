286bd648eb74481672d2863e32417765ea5febec
Karen Palacio <karen.palacio.1994@gmail.com>
2019-09-19 21:58:47 -0300

    Más funcionalidades!
    Ahora el home muestra las últimas páginas agregadas.
    Todavía no implementado mostrar artículo, ni editar página
    (lo saqué del html directamente porque rompía todo).

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,11 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from models import Page
 
 home = Blueprint("home", __name__)
 
+def _page_to_ctx(page):
+    return {
+        "title": page.title,
+        "id": page.id,
+        "slug": page.slug,
+        "created": page.created_at,
+        "cards": [
+            {
+                "content": card.content,
+                "created": card.created_at,
+                "tags": ['definicion', 'historia', 'biografia'],
+            }
+            for card in page.cards
+        ],
+    }
+
+
 @home.route("/")
 @home.route("/home.html")
 def home_main():
-    #TODO: page = Page.query.all().first() 
-    return render_template('home.html')
+    pages = Page.query.order_by(Page.id.desc()).all()
+    context = [_page_to_ctx(page) for page in pages]    
+    return render_template('home.html', pages=context)
 
+
+@home.route("/article/<slug>")
+def article(slug):
+    page = Page.query.filter_by(_slug=slug).first()
+    if not page:
+        abort(404)
+    context = _page_to_ctx(page)
+    return render_template("content.html", **context)
