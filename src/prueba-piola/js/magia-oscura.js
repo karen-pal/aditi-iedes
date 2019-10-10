c07097bc780ad208ad74db20a624f741d7ddad01
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-10-10 18:04:17 -0300

    I was able to make an hamburger menu, my life is complete

--- src/prueba-piola/js/magia-oscura.js
+++ src/prueba-piola/js/magia-oscura.js
@@ -1,9 +1,31 @@
 
-const hamburger = document.querySelector(".hamburger");
+/*const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
-});
+});*/
+var toggles = document.querySelectorAll(".c-hamburger");
 
+for (var i = toggles.length - 1; i >= 0; i--) {
+  var toggle = toggles[i];
+  toggleHandler(toggle);
+};
+
+function toggleHandler(toggle) {
+  toggle.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (this.classList.contains("is-active") === true) {
+      this.classList.remove("is-active");
+      $('.open').removeClass('oppenned');
+    } else {
+      this.classList.add("is-active");
+      $(".open").addClass('oppenned');
+    }
+  });
+}
+$(".sub-menu li a").click(function(event) {
+  $(".open").removeClass('oppenned');
+  $(".c-hamburger").removeClass('is-active');
+});
