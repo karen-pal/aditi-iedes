e417a2c8d6ce4dfab6b182edfe3f3081c95ec22c
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-10-11 15:49:48 -0300

    Agregue mejores versiones de las fotos para mobile y sean responsive. Arregle un bug que se genero al configurar la navbar que de manera random te llevaba al top de la pagina. Cambie la imagen de -DIVERSIDADES- para que pueda acoplarse mejor al layot mobile

--- src/prueba-piola/js/magia-oscura.js
+++ src/prueba-piola/js/magia-oscura.js
@@ -6,6 +6,8 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });*/
+
+/*
 var toggles = document.querySelectorAll(".c-hamburger");
 
 for (var i = toggles.length - 1; i >= 0; i--) {
@@ -28,4 +30,47 @@ function toggleHandler(toggle) {
 $(".sub-menu li a").click(function(event) {
   $(".open").removeClass('oppenned');
   $(".c-hamburger").removeClass('is-active');
-});
+});*/
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#nav"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          500,
+          function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
